a21b33f919db7f882d6e25b1cec30ca4a7aae0d2
Arvind Iyer <arvind@linkeddots.com>
2024-01-16 20:58:06 +0530

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import ChipListInput from "./components/ChipListInput";
 import { useFetchUsers } from "./hooks/useFetchProfileData";
 
 export default function App() {
-
   const { data, loading, error } = useFetchUsers("http://localhost:3000/users");
   return (
     <div className="bg-primary h-screen w-screen p-1 flex gap-2 overflow-hidden">
+      {error && <div>{error}</div>}
       {loading && <div>Loading...</div>}
-      {!loading && <ChipListInput data={data}/>}
+      {!loading && !error && <ChipListInput data={data} />}
     </div>
   );
 }
